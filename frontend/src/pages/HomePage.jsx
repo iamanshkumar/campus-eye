fe66770eb49bf919c24732761e95f4ae6ea5d5c4
Ansh Kumar <anshagroia@gmail.com>
2026-04-06 20:41:48 +0530

    fixed home page css

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import Profile from './Profile.jsx';
 const HomePage = () => {
   const [state , setState] = useState('timeline');
   return (
-    <div className='flex flex-col items-center p-2.5 bg-amber-50'>
+    <div className='flex flex-col items-center p-2.5 bg-amber-50 min-h-screen'>
       <Navbar state={state} setState={setState}></Navbar>
       {state==='timeline' ? <Timeline></Timeline> : state==='profile' ? <Profile></Profile> : <InterviewExperience></InterviewExperience>}
     </div>
