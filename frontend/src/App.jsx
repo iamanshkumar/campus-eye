93682d7ee96bcefbc5e323b1a6a70ff24860de6a
Ansh Kumar <anshagroia@gmail.com>
2026-04-15 01:43:53 +0530

    Added routes protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route ,Navigate } from 'react-router'
 import { Toaster } from 'react-hot-toast'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import LandingPage from './pages/LandingPage'
+import ProtectedRoute from './components/ProtectRoute'
+import { useAuth } from './context/AuthContext'
 
 const App = () => {
+  const { user } = useAuth();
   return (
     <BrowserRouter>
       <Toaster position='bottom-right' />
       <Routes>
         <Route path='/' element={<LandingPage></LandingPage>}></Route>
-        <Route path='/home' element={<HomePage></HomePage>}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
+        <Route 
+          path='/home' 
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/login" 
+          element={user ? <Navigate to="/home" replace /> : <LoginPage />} 
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
